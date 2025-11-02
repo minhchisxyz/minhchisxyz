10ace27a11905980a9fab8c3edab0cecb964e760
Minh Chi Diep <diepminhchi1617@gmail.com>
2025-11-02 15:41:26 +0100

    change opacity, shadow and add backdrop blur

--- app/us/page.tsx
+++ app/us/page.tsx
@@ -2,13 +2,13 @@ import ElapsedTime from "@/app/ui/us/elapsed-time";
 
 export default function Page() {
   const background = `bg-[url('/us/mobile_background.jpg')] md:bg-[url('/us/desktop_background.jpg')] bg-no-repeat bg-cover bg-center bg-fixed`
-  const shadow = 'shadow-[4px_4px_8px_#666666,-4px_-4px_6px_#ffffff]'
+  const shadow = 'shadow-[4px_4px_8px_#222222,-4px_-4px_6px_#ffffff]'
   const hover = 'hover:shadow-none hover:inset-shadow-[-4px_4px_8px_#666666,4px_-4px_6px_#ffffff]'
 
   return (
       <div className={`h-screen w-screen ${background}`}>
         <div className={`absolute bottom-1/10 w-full flex flex-row justify-center`}>
-          <div className={`bg-white/15 rounded-3xl p-5 w-7/10 font-bold text-black shadow-lg ${shadow} ${hover}`}>
+          <div className={`bg-white/30 rounded-3xl p-5 w-7/10 font-bold backdrop-blur-xs text-black ${shadow} ${hover}`}>
             <ElapsedTime/>
           </div>
         </div>
